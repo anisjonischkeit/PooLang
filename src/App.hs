16ccc88e5531e8d21aff1df7f51c480ddf073aff
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2019-01-31 00:40:11 +1000

    made App ready for heroku

--- src/App.hs
+++ src/App.hs
@@ -2,62 +2,61 @@
 
 module App where
 
-import Network.Wai as Wai
-import Network.HTTP.Types (status200, status500) 
-import Network.Wai.Handler.Warp as Warp
-import Data.ByteString.Lazy.Char8 as BL8
-import Data.ByteString.Char8 as B8
-import Data.Monoid ((<>))
-import Data.List.Utils (replace)
-import Data.Maybe (fromMaybe)
-import System.Environment (getEnv, lookupEnv)
+import           Network.Wai                   as Wai
+import           Network.HTTP.Types             ( status200
+                                                , status500
+                                                )
+import           Network.Wai.Handler.Warp      as Warp
+import           Data.ByteString.Lazy.Char8    as BL8
+import           Data.ByteString.Char8         as B8
+import           Data.Monoid                    ( (<>) )
+import           Data.List.Utils                ( replace )
+import           Data.Maybe                     ( fromMaybe )
+import           System.Environment             ( getEnv
+                                                , lookupEnv
+                                                )
 
-import Network.HTTP.Client as NC
-import Network.URI (uriAuthority, uriRegName)
+import           Network.HTTP.Client           as NC
+import           Network.URI                    ( uriAuthority
+                                                , uriRegName
+                                                )
 
 -- <> == mappend
 
 rewritePage :: BL8.ByteString -> String -> String -> BL8.ByteString
 rewritePage page localSite foreignSite = BL8.pack newText
-    where
-        replacedHostname = replace 
-            foreignSite
-            localSite
-            (BL8.unpack page)
-        replacedLogo = replace 
-            "<img src=\"/images/logos/php-logo.svg\" width=\"48\" height=\"24\" alt=\"php\">"
-            "PooLang"
-            replacedHostname
-        newText = replace 
-            "PHP"
-            "PooLang"
-            replacedLogo
-            
+  where
+    replacedHostname = replace foreignSite localSite (BL8.unpack page)
+    replacedLogo     = replace
+        "<img src=\"/images/logos/php-logo.svg\" width=\"48\" height=\"24\" alt=\"php\">"
+        "PooLang"
+        replacedHostname
+    newText = replace "PHP" "PooLang" replacedLogo
+
 buildRequest :: String -> IO NC.Request
 buildRequest url = do
-  nakedRequest <- parseRequest url
-  return (nakedRequest { method = "GET" })
+    nakedRequest <- parseRequest url
+    return (nakedRequest { method = "GET" })
 
 
 fullPath :: Wai.Request -> BL8.ByteString
-fullPath request = 
-    BL8.fromStrict $ 
-        (Wai.rawPathInfo request) <> (Wai.rawQueryString request)
+fullPath request =
+    BL8.fromStrict $ (Wai.rawPathInfo request) <> (Wai.rawQueryString request)
 
 rerouteResponse :: Wai.Request -> String -> String -> IO Wai.Response
 rerouteResponse originalRequest localSite foreignSite = do
     manager <- newManager defaultManagerSettings
-    request <- buildRequest $ "http://" ++ foreignSite ++ (BL8.unpack $  fullPath originalRequest)
+    request <-
+        buildRequest
+        $  "http://"
+        ++ foreignSite
+        ++ (BL8.unpack $ fullPath originalRequest)
     response <- httpLbs request manager
 
     return $ Wai.responseLBS
         status200
         ([]) -- This should be replaced with some of php's response headers (ie. Content-Type) 
-        (rewritePage 
-            (NC.responseBody response)
-            localSite
-            foreignSite
-        )
+        (rewritePage (NC.responseBody response) localSite foreignSite)
 
 noEnvResponse :: String -> IO Wai.Response
 noEnvResponse envName = return $ Wai.responseLBS
@@ -69,13 +68,14 @@ app :: Wai.Application
 app request respond = do
     Prelude.putStrLn "I've done some IO here"
 
-    maybeLocalSite <- lookupEnv "LOCAL_SITE"
+    maybeLocalSite   <- lookupEnv "LOCAL_SITE"
     maybeForeignSite <- lookupEnv "FOREIGN_SITE"
 
-    response <- case (maybeLocalSite, maybeForeignSite) of 
-        (Just localSite, Just foreignSite) -> rerouteResponse request localSite foreignSite
-        (Nothing, _) -> noEnvResponse "LOCAL_SITE"
-        (_, Nothing) -> noEnvResponse "FOREIGN_SITE"
+    response         <- case (maybeLocalSite, maybeForeignSite) of
+        (Just localSite, Just foreignSite) ->
+            rerouteResponse request localSite foreignSite
+        (Nothing, _      ) -> noEnvResponse "LOCAL_SITE"
+        (_      , Nothing) -> noEnvResponse "FOREIGN_SITE"
 
     respond response
 
@@ -83,4 +83,4 @@ run :: IO ()
 run = do
     port <- getEnv "PORT"
     Prelude.putStrLn $ "http://localhost:" ++ port
-    Warp.run (read 8080) app
+    Warp.run (read port) app
